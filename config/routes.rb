da91ee218e64d829752b5d76cac662c9e5417a55
tadayuki0120@icloud.com <ec2-user@ip-172-31-35-205.ap-southeast-1.compute.internal>
2021-04-12 06:08:37 +0000

    [Finish]デバイスを用いてログイン機能の実

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'homes#top'
+  devise_for :customers
+  resources :customers
 end
